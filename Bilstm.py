86dae71e63adcdf492adcb9f25f6c42e6bffcf47
silver <mj_ol@outlook.com>
2019-11-23 12:48:17 +0900

    UPDATE

--- Bilstm.py
+++ Bilstm.py
@@ -1,2 +1,16 @@
 import tensorflow as tf
 import jieba
+import numpy as np
+
+from tensorflow.contrib.crf import crf_log_likehood
+from tensorflow.contrib.crf import viterbi_decode
+from tensorflow.contrib.rnn import LSTMCell
+
+class BiLSTM_CRF():
+    def __init__(self):
+    """Init paramaters"""
+        self.batch_size = 0
+        self.epoch = 0
+        self.hidden_dim = 0
+        self.embeddings = 0
+    def 
