5420dc69e7322b0dcf0dd4b9948392f17fc9f764
silver <mj_ol@outlook.com>
2019-11-25 21:02:55 +0900

    UPDATE: Update look up

--- Bilstm.py
+++ Bilstm.py
@@ -6,17 +6,21 @@ from tensorflow.contrib.crf import crf_log_likehood
 from tensorflow.contrib.crf import viterbi_decode
 from tensorflow.contrib.rnn import LSTMCell
 
+import Parameters as pm
 class BiLSTM_CRF():
-    def __init__(self):
+    def __init__(self, ):
     """Init paramaters"""
         
     def build_model(self):
         # placeholder
+        # setting up chars ids and words ids
         self.word_ids = tf.placeholder(tf.int32, shape = [None, None], name = "word_ids")
-        self.labels = tf.placeholder(tf.int32, shape = [None, None], name = "labels")
         self.sequence_lengths = tf.placeholder(tf.int32, shape = [None], name = "sequence_lengths")
+        
+        self.labels = tf.placeholder(tf.int32, shape = [None, None], name = "labels")
         self.dropout = tf.placeholder(tf.int32, shape = [None], name = "dropout")
 
         # build look up layer
         with tf.variable_scope("lookup"):
-            
+            embedding = tf.Variable(self.embeddings, dtype = tf.float32, traina)
+
